e02fca1d406e1cceab852e49cdbc25d816e9d2e6
CristianBorreDev <crstnborre@gmail.com>
2025-12-03 15:43:33 -0500

    Preparando workflow de deploy

--- src/app.js
+++ src/app.js
@@ -11,10 +11,15 @@ app.use(express.json());
 // Rutas
 const appointmentsRoutes = require("./routes/appointments.routes");
 
+app.use("/", (req, res) => {
+    res.json({ status: 200, message: "Api corriendo correctamente en el puerto: 3000" })
+})
+
 app.use("/api/appointments", appointmentsRoutes);
 
 app.get("/api/health", (req, res) => {
   res.json({ status: "API OK" });
 });
+
 
 module.exports = app;
